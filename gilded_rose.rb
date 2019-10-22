f99181ee21a7cd89f5dec6f282dce65ec2388f42
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-22 18:37:50 +0100

    Delete lines of code for conditio of item.sell_in < 0

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -27,25 +27,6 @@ class GildedRose
       else
         item.quality -= 1 if item.quality > MIN_QUALITY
       end
-
-      
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
     end
   end
 
